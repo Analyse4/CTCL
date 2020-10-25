11c03ce58366284653d88bcabf7cc640034ac550
Analyse4 <analysek4@gmail.com>
2020-10-25 16:44:28 +0800

    make onedotfourgraceful

--- 1/onedotfour/onedotfour.go
+++ 1/onedotfour/onedotfour.go
@@ -6,17 +6,17 @@ import "strings"
 func IsPalindromePermutation(s string) bool {
 	s = strings.Replace(s, " ", "", -1)
 	s = strings.ToLower(s)
-	count := 0
+	foundOdd := false
 	st := make(map[rune]int, len(s))
 	for _, c := range []rune(s) {
 		st[c]++
 	}
 	for _, v := range st {
 		if v%2 != 0 {
-			count++
-			if count > 1 {
+			if foundOdd {
 				return false
 			}
+			foundOdd = true
 		}
 	}
 	return true
